2a758be35c5f5e94f0ad2fb5c4ccbc062c32debf
YasuChiba <yasu.bv0309@gmail.com>
2020-11-17 11:07:36 +0000

    update

--- run.sh
+++ run.sh
@@ -1,20 +1,21 @@
 #!/bin/sh
 set -e
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
 
 
 apt -y update 
 apt -y upgrade
 
-apt install -y git vim cmake build-essential python3 python3-pip openjdk-8-jdk unzip wget
+apt install -y git vim cmake build-essential python3 python3-pip openjdk-8-jdk unzip wget gradle
 
 cd /usr/bin
 ln -s /usr/bin/python3.6 ./python 
-cd ~/Env_test
 
+cd $SCRIPT_DIR
 pip3 install vcstool catkin-pkg empy
 
 
-./install_android_env.sh
-./install_ament.sh
-./install_ros2_java.sh
-./install_micro_xrce_dds.sh
+$SCRIPT_DIR/install_android_env.sh
+$SCRIPT_DIR/install_ament.sh
+$SCRIPT_DIR/install_ros2_java.sh
+$SCRIPT_DIR/install_micro_xrce_dds.sh
